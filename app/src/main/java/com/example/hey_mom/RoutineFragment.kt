cfc49ff2676315eda67c0a8585894949d71f4871
Nyubenlo Seb <nyubenlo18@gmail.com>
2025-05-20 18:09:50 +0530

    update

--- app/src/main/java/com/example/hey_mom/RoutineFragment.kt
+++ app/src/main/java/com/example/hey_mom/RoutineFragment.kt
@@ -4,14 +4,18 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.GridLayout
 import android.widget.GridView
+import android.widget.ImageView
 import android.widget.Spinner
+import android.widget.TextView
 import android.widget.TimePicker
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -20,7 +24,10 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.navigation.NavigationView
 
-
+data class GridItem(
+    val title: String,
+    val iconRes: Int
+)
 class RoutineFragment : Fragment() {
 
     private lateinit var drawerLayout: DrawerLayout
@@ -36,18 +43,36 @@ class RoutineFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_routine, container, false)
 
 
-        //For GridView
-        val gridView = view.findViewById<GridView>(R.id.gridView)
+        val gridLayout = view.findViewById<GridLayout>(R.id.gridLayout)
 
         val itemList = listOf(
-            GridItem("Sleep",R.drawable.babysleep),
-            GridItem("Meal",R.drawable.bottle),
-            GridItem("Sleep",R.drawable.babysleep),
-            GridItem("Diaper",R.drawable.diaper)
+            GridItem("Sleep", R.drawable.babysleep),
+            GridItem("Meal", R.drawable.bottle),
+            GridItem("Sleep", R.drawable.babysleep),
+            GridItem("Diaper", R.drawable.diaper)
         )
 
-        val adapter = GridAdapter(requireContext(), itemList)
-        gridView.adapter = adapter
+        for (item in itemList) {
+            val itemView = LayoutInflater.from(requireContext())
+                .inflate(R.layout.grid_item, gridLayout, false)
+
+            val textView = itemView.findViewById<TextView>(R.id.textView)
+            val imageView = itemView.findViewById<ImageView>(R.id.item_image)
+
+            textView.text = item.title
+            imageView.setImageResource(item.iconRes)
+
+            val params = GridLayout.LayoutParams().apply {
+                width = 0
+                height = GridLayout.LayoutParams.WRAP_CONTENT
+                columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+                setMargins(16, 16, 16, 16)
+            }
+
+            gridLayout.addView(itemView, params)
+        }
+
+
         return view
     }
 
