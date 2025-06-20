b35e3ce0941222e53a71537c4098a05d5b195018
Nyubenlo Seb <nyubenlo18@gmail.com>
2025-06-20 17:23:10 +0530

    updated

--- app/src/main/java/com/example/hey_mom/ui/OverviewFragment.kt
+++ app/src/main/java/com/example/hey_mom/ui/OverviewFragment.kt
@@ -392,6 +392,10 @@ class OverviewFragment : Fragment() {
                         val intent = Intent(requireContext(), Vaccine_Activity::class.java)
                         startActivity(intent)
                     }
+                    R.id.emergency -> {
+                        val intent = Intent(requireContext(), emergency::class.java)
+                        startActivity(intent)
+                    }
 
                 }
                 drawerLayout.closeDrawer(GravityCompat.START)
